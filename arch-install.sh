4bd693d78aecf120873cfa28b932252b79f6628a
Your Name <you@example.com>
2021-11-09 13:58:33 -0600

    adding

---
+++ arch-install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+echo "#####################################################################"
+echo "################## Updateing Mirrors ################################"
+echo "#####################################################################"
+
+sudo pacman -S reflector
+
+sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist
+
+echo "#####################################################################"
+echo "################## Updateing Mirrors Done############################"
+echo "#####################################################################"
+
+echo "###########################################################"
+echo "################## Installing arch linux ##################"
+echo "###########################################################"
+
+timedatectl set-ntp true
+
+mkfs.fat -F32 /dev/sda1
+
+mkfs.ext4 /dev/sda2
+
+mkswap /dev/sda3
+
+mount /dev/sda2 /mnt
+
+swapon /dev/sda3
+
+pacstrap /mnt base linux linux-firmware base-devel git
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "#########################################################################"
+echo "################## Copying Scripts To Installed System ##################"
+echo "#########################################################################"
+
+cd ..
+
+cp -r Arch-install-desktop /mnt/home/
+
+
+echo "##########################################"
+echo "################## Done ##################"
+echo "##########################################"
+
+
+arch-chroot /mnt /mnt/home/install-desktop.sh
