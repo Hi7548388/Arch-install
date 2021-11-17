ab79f228f0d6b4a83f820d508efa3663bedeb759
Hi7548388 <93167425+Hi7548388@users.noreply.github.com>
2021-11-17 09:55:26 -0600

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -27,15 +27,27 @@ mkfs.fat -F32 /dev/sda1
 
 mkfs.ext4 /dev/sda2
 
-mkswap /dev/sda3
-
 mount /dev/sda2 /mnt
-
-swapon /dev/sda3
 
 pacstrap /mnt base linux linux-firmware base-devel git
 
 genfstab -U /mnt >> /mnt/etc/fstab
+
+
+mkdir /mnt/opt/swap
+
+dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
+
+chmod 600 /mnt/opt/swap/swapfile
+
+chown root /mnt/opt/swap/swapfile
+
+mkswap /mnt/opt/swap/swapfile
+swapon /mnt/opt/swap/swapfile
+
+" /swapfile none swap defaults 0 0 "
+
+echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab #Add swap to fstab, so it KEEPS working after installation.
 
 echo "#########################################################################"
 echo "################## Copying Scripts To Installed System #############################"
